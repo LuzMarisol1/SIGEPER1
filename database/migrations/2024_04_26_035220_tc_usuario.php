8d0bb96807df244760fe53f025bc3316649a950a
Luz Marisol <78156208+LuzMarisol1@users.noreply.github.com>
2024-04-25 22:11:03 -0600

    Migración de 2024_04_26_035220_tc_usuario

---
+++ database/migrations/2024_04_26_035220_tc_usuario.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TcUusuario', function (Blueprint $table){
+            $table->increments('idTcUsuario');
+            $table->string('descripcion', 255);
+            $table->charset = 'utf8mb4'; // Establecer el conjunto de caracteres
+            $table->collation = 'utf8mb4_unicode_ci'; // Establecer la intercalación
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Shema::dropIfExits('TcUsuarios');
+    }
+};
