4f64549c376f6327663194fed9dd1c41552b91da
Luz Marisol <78156208+LuzMarisol1@users.noreply.github.com>
2024-04-25 22:55:23 -0600

    Create 2024_04_26_045016_tc_documentacion.php

---
+++ database/migrations/2024_04_26_045016_tc_documentacion.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TcDocumentacion', function (Blueprint $table) {
+            $table->increments('idTcDocumentacion');
+            $table->string('descripcion', 255);
+            $table->timestamps(); // Si deseas agregar timestamps
+            $table->engine = 'InnoDB'; // Establecer el motor de almacenamiento
+            $table->charset = 'utf8mb4'; // Establecer el conjunto de caracteres
+            $table->collation = 'utf8mb4_unicode_ci'; // Establecer la intercalación
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('TcDocumentacion');
+    }
+};
