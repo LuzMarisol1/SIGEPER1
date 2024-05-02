fa1c182b64680458975d19bdc82244a0a4864189
Luz Marisol <78156208+LuzMarisol1@users.noreply.github.com>
2024-05-01 23:09:34 -0600

    Update tablaAlumnos.blade.php

--- resources/views/tablaAlumnos.blade.php
+++ resources/views/tablaAlumnos.blade.php
@@ -6,18 +6,24 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!--JQUERY-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--CSS-->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    
+    <!-- SweetAlert2-->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <!--PERSONALIZADO-->
     <link href="css/tablas.css" rel="stylesheet" />
+    
     <script type="text/javascript" src="js/tablaAlumnos.js"></script>
+
     <title>Proyectos de Experiencia Recepcional</title>
 </head>
+
+
 
 <body>
     <header id="main-header">
@@ -27,12 +33,12 @@
             </span>
         </a>
     </header>
-    
+
     <div id="tablaA" data-search="true" data-filter-control="true">
         <div class="container">
             <h1 style="text-align: center"> Proyectos de Experiencia Recepcional</h1>
             <h3 style="text-align: center">Seguimiento de los proyectos de Experiencia Recepcional</h3>
-            
+
             <table id="tabAlumnos" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
                     <tr>
@@ -58,13 +64,13 @@
                         <td></td>
                         <td></td>
                         <td>
-                        <button type="button" class="btn btn-primary btn-xs dt-edit" onclick="abrirModal()"
-                            style="margin-right:16px;">
-                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                        </button>
-                        
-                    </td>
-                </tr>
+                            <button type="button" title="Abrir un modal con más información" id="btnEditar" class="btn btn-primary btn-xs dt-edit"
+                                style="margin-right:16px;">
+                                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            </button>
+
+                        </td>
+                    </tr>
                 </tbody>
             </table>
             <nav aria-label="Page navigation example">
@@ -80,69 +86,60 @@
                     </li>
                 </ul>
             </nav>
-            <div id="mODAL" class="modal" tabindex="1" role="dialog">
-                <div class="modal-dialog" role="document">
+            <div id="mODAL" class="modal" tabindex="-1">
+                <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title">Editar Datos</h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
+                            <h5 class="modal-title">
+                                <h3>Editar datos</h3>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close">Cancelar</button>
                         </div>
                         <div class="modal-body">
-                            <form id="form-editar">
-                                <div class="select-container">
-                                    <label for="inscripcion"><b>Inscripción:</b></label>
-                                    <select id="inscripcion" name="inscripcion" class="form-select form-select-sm"
-                                        aria-label=".form-select-sm example">
-                                        <option value="">Seleccionar</option>
-                                        <option value="1">Primera </option>
-                                        <option value="2">Continuidad</option>
-                                        <option value="2">Segunda</option>
+                            <form id="formEditar">
+                                <div id="tipoInsc" class="mb-3">
+                                    <label class="form-label">Tipo de inscripción</label>
+                                    <select id="selectTipoInscripcion" class="form-select "
+                                        aria-label="Default select example">
+                                        <option value="1">Primera Inscripción</option>
+                                        <option value="2">Continudad</option>
+                                        <option value="3">Segunda Inscripción</option>
                                     </select>
                                 </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="proyecto"><b>Proyecto:</b></label>
-                                    <input type="text" id="proyecto" class="form-control" name="proyecto"
-                                        placeholder="Nombre del proyecto">
+                                <div class="mb-3">
+                                    <label for="tituloProyecto" class="form-label">Proyecto</label>
+                                    <input id="tituloProyecto" type="text" class="form-control">
                                 </div>
-                                <br />
-                                <div class="select-container">
-                                    <label for="estatus"><b>Modalidad:</b></label>
-                                    <select id="estatus" name="estatus" class="form-select form-select-sm"
-                                        aria-label=".form-select-sm example">
-                                        <option value="pendiente">Proyecto</option>
-                                        <option value="proceso">Examen</option>
-                                        <option value="terminado">Ceneval</option>
+                                <div class="mb-3">
+                                    <label class="form-label">Modalidad</label>
+                                    <select id ="selectModalidad" class="form-select "
+                                        aria-label="Default select example">
+                                        <option selected>Seleccionar</option>
+                                        <option value="1">Trabajo Recepcional</option>
+                                        <option value="2">Examen CENEVAL</option>
+                                        <option value="3">Promedio</option>
                                     </select>
                                 </div>
-                                <div class="form-group">
-                                    <label for="director"><b>Director:</b></label>
-                                    <input type="text" id="director" class="form-control" name="director"
-                                        placeholder="Nombre del director">
-                                </div>
-                                <div class="select-container">
-                                    <label for="estatus"><b>Estatus:</b></label>
-                                    <select id="estatus" name="estatus" class="form-select form-select-sm"
-                                        aria-label=".form-select-sm example">
-                                        <option value="pendiente">Pendiente</option>
-                                        <option value="proceso">En proceso</option>
-                                        <option value="terminado">Terminado</option>
-                                    </select>
-                                </div>
-                                <br>
-                                <br>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary">Guardar</button>
-                                    <button type="button"
-                                        class="btn btn-secondary"data-bs-dismiss="modal">Cerrar</button>
+                                <div class="mb-3">
+                                    <label class="form-label">Director</label>
+                                    <input id="directorP" type="text" class="form-control">
                                 </div>
 
                             </form>
+                            <div id="botonesEdit" class="modal-footer">
+                                <button id="guardarDatos"type="button" class="btn btn-primary"
+                                    onclick="actualizarDatos()">Guardar</button>
+                                <button type="button"
+                                    class="btn btn-secondary"data-bs-dismiss="modal">Cancelar</button>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </body>
+
+</html>
